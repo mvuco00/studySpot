c2cb66db99652bde8776bb9f9ad3fd61191376c5
mvuco00 <mvuco00@fesb.hr>
2021-02-11 15:21:41 +0100

    when task is finnished

--- studyspot/src/Components/ToDo/Task/task.js
+++ studyspot/src/Components/ToDo/Task/task.js
@@ -1,8 +1,24 @@
 import React, { Component } from "react";
-
+import classes from "./task.css";
 class Task extends Component {
+  state = {
+    done: false,
+    style: classes.task,
+  };
+  handleClick = () => {
+    if (!this.state.done) {
+      this.setState({ done: true, style: classes.done });
+    } else {
+      this.setState({ done: false, style: classes.task });
+    }
+  };
+
   render() {
-    return <div>{this.props.item}</div>;
+    return (
+      <div className={this.state.style} onClick={this.handleClick}>
+        {this.props.item}
+      </div>
+    );
   }
 }
 
