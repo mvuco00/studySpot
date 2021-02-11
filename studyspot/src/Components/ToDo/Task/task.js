551d7ebe9e8e25f8cc743d732ce0fe78404b0c92
mvuco00 <mvuco00@fesb.hr>
2021-02-11 15:07:40 +0100

    ToDo list

---
+++ studyspot/src/Components/ToDo/Task/task.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class Task extends Component {
+  render() {
+    return <div>{this.props.item}</div>;
+  }
+}
+
+export default Task;
--- studyspot/src/Components/ToDo/todo.js
+++ studyspot/src/Components/ToDo/todo.js
@@ -1,11 +1,48 @@
-import React from "react";
+import React, { Component } from "react";
 import AuxComp from "../../HOC/auxcomp";
+import Task from "./Task/task";
 
-const ToDo = (props) => (
-  <AuxComp>
-    <div>TODO</div>
-    <div>{props.children}</div>
-  </AuxComp>
-);
+class ToDo extends Component {
+  state = {
+    todoList: ["Kupit mlijeko", "Hustlat"],
+    userInput: "",
+  };
+  handleInput = (e) => {
+    this.setState({
+      userInput: e.target.value,
+    });
+  };
 
+  handleSubmit = (e) => {
+    e.preventDefault();
+    if (this.state.userInput != "") {
+      this.setState({
+        todoList: [this.state.userInput, ...this.state.todoList],
+        userInput: "",
+      });
+    }
+  };
+  render() {
+    return (
+      <AuxComp>
+        <div>TODO</div>
+        <form autoComplete="off" onSubmit={this.handleSubmit}>
+          <input
+            type="text"
+            placeholder="ToDo..."
+            value={this.state.userInput}
+            onChange={this.handleInput}
+          />
+          <input type="submit" value="Dodaj" />
+        </form>
+
+        <div>
+          {this.state.todoList.map((item) => {
+            return <Task item={item} key={item} />;
+          })}
+        </div>
+      </AuxComp>
+    );
+  }
+}
 export default ToDo;
